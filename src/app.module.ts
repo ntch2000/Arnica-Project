a5aacedb606f6deccf99be201e3e9effc10edfbf
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2024-10-21 22:18:06 -0400

    created module, controller and service base files for gitleaks scanning

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RepoModule } from './repo/repo.module';
+import { GitleaksModule } from './gitleaks/gitleaks.module';
+
 
 
 @Module({
-  imports: [RepoModule],
+  imports: [RepoModule, GitleaksModule],
   controllers: [AppController],
   providers: [AppService],
 })
